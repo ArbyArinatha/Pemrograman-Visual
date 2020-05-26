f842e133568b39d0368f7e903c515f832b141f6b
ArbyArinatha <arbyarinatha21@gmail.com>
2020-05-26 22:03:22 +0800

    React Part 1 & 2
    Tugas Visual React Part 1 & 2

---
+++ Tugas Visual React/Part 1 & 2/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Sidebar from "./components/Sidebar"
+import CreateAppointment from "./components/appointment/Create"
+import AppointmentList from "./components/appointment/List"
+import TodayAppointment from "./components/appointment/Today"
+
+function App() {
+  return (
+    <div>
+      <Sidebar />
+      <CreateAppointment />
+      <AppointmentList />
+      <TodayAppointment />
+    </div>
+  );
+}
+
+export default App;
